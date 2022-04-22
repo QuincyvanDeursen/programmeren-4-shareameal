ce8599937746d752d3e56714bbb496b04b95872f
Quincy van Deursen <q.vandeursen@student.avans.nl>
2022-04-22 19:36:54 +0200

    Opdracht 1: UC-201-t/m- UC206 added

---
+++ data.js
@@ -0,0 +1,24 @@
+module.exports = [
+  (user1 = {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    street: "Lovendijkstraat 61",
+    city: "Breda",
+    isActive: true,
+    emailAdress: "John.Doe@server.com",
+    password: "secret",
+    phoneNumber: "06-11223344",
+  }),
+  (user2 = {
+    id: 2,
+    firstName: "John2",
+    lastName: "Doe2",
+    street: "Lovendijkstraat 61332",
+    city: "Breda",
+    isActive: false,
+    emailAdress: "John.Doe@server.com2",
+    password: "Dog",
+    phoneNumber: "06-11223344",
+  }),
+];
