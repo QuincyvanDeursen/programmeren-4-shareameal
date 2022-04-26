0a7857075d3e5f7bc5d9de517114d8474d2b50c0
Quincy van Deursen <q.vandeursen@student.avans.nl>
2022-04-26 12:03:37 +0200

    added: testing libraries

---
+++ test/movie.controller.test.js
@@ -0,0 +1,10 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../index");
+
+chai.should();
+chai.use(chaiHttp);
+
+describe("UC-101 Login", () => {
+  describe();
+});
