0a25980c82499d759ce8c3d0b00ac1e50c9cc6c6
Quincy van Deursen <q.vandeursen@student.avans.nl>
2022-04-26 11:52:12 +0200

    Splitted logic from routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const { updateUser } = require("../Controller/userController");
 const router = express.Router();
-let database = [];
-let id = 0;
+const userController = require("../Controller/userController");
 
 //this function executes a GET call on the homepage.
 router.get("/", (req, res) => {
@@ -12,134 +12,21 @@ router.get("/", (req, res) => {
 });
 
 //this function executes a post call to create an user. The given email must be unique. The user is added to the database (array). UC-201.
-router.post("/api/user", (req, res) => {
-  let user = req.body;
-  let email = user.emailAdress;
-  let userArray = database.filter((item) => item.emailAdress == email);
-  if (userArray.length > 0) {
-    console.log(user);
-    res.status(401).json({
-      status: 401,
-      result: "Email already in use",
-    });
-  } else {
-    id++;
-    user = {
-      id,
-      ...user,
-    };
-    database.push(user);
-    res.status(201).json({
-      status: 201,
-      result: " gelukt",
-    });
-  }
-});
+router.post("/api/user", userController.addUser);
 
 //function to update an existing user. Email cant be duplicated. UC-205
-router.put("/api/user/:userId", (req, res) => {
-  //get the user for the given ID.
-  let oldUserArray = database.filter((item) => item.id == req.params.userId);
-  //if an user is found, create a user with the new req data to replace the old with the new user in the database
-  if (oldUserArray.length > 0) {
-    let oldUser = oldUserArray[0];
-    let id = oldUser.id;
-    let newUser = {
-      id,
-      ...req.body,
-    };
-    //filter to find emails addresses that match the (new) given emailaddress.
-    let newEmail = req.body.emailAdress;
-    let emails = database.filter(
-      (item) => item.emailAdress.toLowerCase() == newEmail.toLowerCase()
-    );
-    //if the new email matches with one of the emails in the database. emails.length should be greather then 0.
-    if (emails.length > 0) {
-      // if the email matches with one of the other users emails, then an error is given.
-      if (req.params.userId != emails[0].id) {
-        res.status(400).json({
-          Status: 400,
-          Message: `${newEmail} is already in use`,
-        });
-        // if the id of the req user is the same as the id of the matching user, then it is allowed to update the email.
-      } else {
-        database.splice(database.indexOf(oldUser), 1, newUser);
-        res.status(200).json({
-          Status: 200,
-          Message: `user with id ${oldUser.id} has been updated`,
-        });
-      }
-      // if there isnt a match between the newUser email with old Users emails, it is allowed to update the user.
-    } else {
-      database.splice(database.indexOf(oldUser), 1, newUser);
-      res.status(200).json({
-        Status: 200,
-        Message: `user with id ${oldUser.id} has been updated`,
-      });
-    }
-    //if there is no user found for the given id, then give an error.
-  } else {
-    res.status(400).json({
-      Status: 400,
-      Message: `user with id ${oldUser.id} can not be found`,
-    });
-  }
-});
+router.put("/api/user/:userId", userController.updateUser);
 
 //function to get the profile. UC-203
-router.get("/api/user/profile", (req, res) => {
-  res.status(401).json({
-    status: 401,
-    result: `End-point not created yet.`,
-  });
-});
+router.get("/api/user/profile", userController.getProfile);
 
 //function to find an user by id. UC-204
-router.get("/api/user/:userId", (req, res) => {
-  const userId = req.params.userId;
-  console.log(`User met ID ${userId} gezocht`);
-  let userArray = database.filter((item) => item.id == userId);
-  if (userArray.length > 0) {
-    let user = userArray[0];
-    console.log(user);
-    res.status(200).json({
-      status: 200,
-      result: user,
-    });
-  } else {
-    res.status(401).json({
-      status: 401,
-      result: `User with ID ${userId} not found`,
-    });
-  }
-});
+router.get("/api/user/:userId", userController.findUser);
 
 //function to get all users in the database (array). UC-202
-router.get("/api/user", (req, res) => {
-  res.status(200).json({
-    status: 200,
-    result: database,
-  });
-});
+router.get("/api/user", userController.getAllUsers);
 
 //function to delete a specific user.
-router.delete("/api/user/:userId", (req, res) => {
-  let user = req.params.userId;
-  let userArray = database.filter((item) => item.id == user);
-  if (userArray.length > 0) {
-    let userToDelete = userArray[0];
-    database.splice(database.indexOf(userToDelete), 1);
-    console.log(userToDelete);
-    res.status(200).json({
-      status: 200,
-      result: `User with id ${user} succesfully deleted`,
-    });
-  } else {
-    res.status(401).json({
-      status: 401,
-      result: `User with ID ${user} not found`,
-    });
-  }
-});
+router.delete("/api/user/:userId", userController.deleteUser);
 
 module.exports = router;
