1b4379163a3eb4443c1cef1ecb9ec8df6e1e33f7
Quincy van Deursen <q.vandeursen@student.avans.nl>
2022-05-21 23:02:06 +0200

    test fix

--- test/integration/meal.controller.test.js
+++ test/integration/meal.controller.test.js
@@ -119,7 +119,7 @@ describe("meals", () => {
           isVega: true,
           isVegan: true,
           isToTakeHome: true,
-          dateTime: "2022-05-21T07:11:46.701Z",
+          dateTime: "2022-05-21 07:11:46",
           imageUrl:
             "https://miljuschka.nl/wp-content/uploads/2021/02/Pasta-bolognese-3-2.jpg",
           allergenes: ["gluten", "lactose"],
@@ -139,7 +139,7 @@ describe("meals", () => {
             isVega: true,
             isVegan: true,
             isToTakeHome: true,
-            dateTime: "2022-05-21T07:11:46.701Z",
+            dateTime: "2022-05-21 07:11:46",
             imageUrl:
               "https://miljuschka.nl/wp-content/uploads/2021/02/Pasta-bolognese-3-2.jpg",
             allergenes: result.allergenes,
